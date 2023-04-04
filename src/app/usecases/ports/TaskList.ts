fcc376edeb9ec7af13eb4dfccf1b21e32c54976f
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-04-03 23:17:41 -0300

    feat: update TaskList interface

--- src/app/usecases/ports/TaskList.ts
+++ src/app/usecases/ports/TaskList.ts
@@ -1,10 +1,42 @@
 import { Task, TaskObject } from "../../entities";
 
 export default interface TaskList {
-  addTask(task: TaskObject): Promise<void>;
+  /**
+   * Add a new task to the task list.
+   * @param {Task} task - The task to add.
+   * @returns {Task} - The task added.
+  */
+  addTask(task: TaskObject): Promise<TaskObject>;
+
+  /**
+ * Remove a task from the task list by its text.
+ * @param {string} task_id - The text of the task to remove.
+ */
   removeTask(task_id: string): Promise<void>;
-  editTask(task_id: string): Promise<void>;
+
+  /**
+   * Edit a task from the task list by its text.
+   * @param {string} task_id - The text of the task to edit.
+   * @param {string} text - The new text of the task.
+  */
+  editTask(task_id: string, text: string): Promise<void>;
+
+  /**
+   * Mark a task as done from the task list by its text.
+   * @param {string} task_id - The text of the task to mark as done.
+  */
   markTaskAsDone(task_id: string): Promise<void>;
+
+  /**
+   * Mark a task as undone from the task list by its text.
+   * @param {string} task_id - The text of the task to mark as undone.
+  */
   markTaskAsUndone(task_id: Task): Promise<void>;
-  getTasks(): Promise<TaskObject[]>;
+
+  /**
+   * Get all tasks from the task list.
+    * @returns {Task[]} - The list of tasks.
+    * @returns {[]} - If there are no tasks.
+  */
+  getTasks(): Promise<TaskObject[] | []>;
 }
