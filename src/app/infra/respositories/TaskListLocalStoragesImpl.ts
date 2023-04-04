3e10ad319d0dfd192d448eeab8437fe5638e68df
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-04-03 23:58:05 -0300

    refact tasklist implementation

--- src/app/infra/respositories/TaskListLocalStoragesImpl.ts
+++ src/app/infra/respositories/TaskListLocalStoragesImpl.ts
@@ -4,57 +4,61 @@ import TaskList from "../../usecases/ports/TaskList";
 import { TaskObject } from "../../entities";
 
 export default function TaskListLocalStoragesImpl(): TaskList {
-  const FIELD_NAME = "tasks";
+  const STORAGE_KEY = "tasks";
 
-  const getTasks = async (): Promise<TaskObject[]> => {
-    const tasks = localStorage.getItem("tasks");
-    if (tasks) {
-      return JSON.parse(tasks);
-    }
-    return [];
-  }
+  const getStoredTasks = (): TaskObject[] => {
+    const tasks = localStorage.getItem(STORAGE_KEY);
+    return tasks ? JSON.parse(tasks) : [];
+  };
+
+  const saveTasks = (tasks: TaskObject[]): void => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+  };
 
   return {
     addTask: async (task: TaskObject): Promise<void> => {
-      const tasks = await getTasks();
+      const tasks = getStoredTasks();
       tasks.push(task);
-      localStorage.setItem(FIELD_NAME, JSON.stringify(tasks));
+      saveTasks(tasks);
     },
     removeTask: async (task_id: string): Promise<void> => {
-      const tasks = await getTasks();
+      const tasks = getStoredTasks();
       const newTasks = tasks.filter((task) => task.text !== task_id);
-      localStorage.setItem(FIELD_NAME, JSON.stringify(newTasks));
+      saveTasks(newTasks);
     },
     editTask: async (task_id: string, text: string): Promise<void> => {
-      const tasks = await getTasks();
+      const tasks = getStoredTasks();
       const newTasks = tasks.map((task) => {
         if (task.text === task_id) {
           task.text = text;
         }
         return task;
       });
-      localStorage.setItem(FIELD_NAME, JSON.stringify(newTasks));
+      saveTasks(newTasks);
     },
     markTaskAsDone: async (task_id: string): Promise<void> => {
-      const tasks = await getTasks();
+      const tasks = getStoredTasks();
       const newTasks = tasks.map((task) => {
         if (task.text === task_id) {
           task.done = true;
         }
         return task;
       });
-      localStorage.setItem(FIELD_NAME, JSON.stringify(newTasks));
+      saveTasks(newTasks);
     },
     markTaskAsUndone: async (task_id: string): Promise<void> => {
-      const tasks = await getTasks();
+      const tasks = getStoredTasks();
       const newTasks = tasks.map((task) => {
         if (task.text === task_id) {
           task.done = false;
         }
         return task;
       });
-      localStorage.setItem(FIELD_NAME, JSON.stringify(newTasks));
+      saveTasks(newTasks);
     },
-    getTasks
+    getTasks: async (): Promise<TaskObject[] | []> => {
+      const tasks = getStoredTasks();
+      return tasks;
+    }
   }
 }
